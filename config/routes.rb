7db34c4331f3df0396ba14e41c5fece6de18708b
Maxence Laplante <maxence.laplante@gmail.com>
2021-06-11 15:16:43 +0200

    watch list

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lists, only: [ :index, :show, :new, :create, :destroy ] do 
+    resources :bookmarks, only: [ :new, :create ]
+    # resources :movies, only: [ :index, :show, :new, :create, :edit]
+  end
+  resources :bookmarks, only: [ :destroy ]
+  root to: "lists#index"
 end
+# localhost:3000/bookmarks
+# localhost:3000/list/2/bookmarks
